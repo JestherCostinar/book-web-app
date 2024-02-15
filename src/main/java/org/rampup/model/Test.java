c33a9acd95f6a1867d30784ae3af1bd4f5d9d930
Jesther Costinar <jesther.jc15@gmail.com>
2024-02-15 19:29:04 +0800

    JPA configure

--- src/main/java/org/rampup/model/Test.java
+++ src/main/java/org/rampup/model/Test.java
@@ -1,12 +1,22 @@
 package org.rampup.model;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Test {
 
 	public static void main(String[] args) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("book_pu");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		
 		Book book = new Book();
-		book.setId(1);
 		book.setTitle("Title");
 		
+		entityManager.persist(book);
+		entityManager.getTransaction().commit();
 		System.out.println(book.toString());
 	}
 
